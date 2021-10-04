525cbd385eb6aae8e877cdc9231f575163614511
Ronald Mannak <ronald@starlingprotocol.com>
2021-10-03 17:59:51 -0700

    Add check state for MacOS version

--- macOS (App)/AppDelegate.swift
+++ macOS (App)/AppDelegate.swift
@@ -6,12 +6,30 @@
 //
 
 import Cocoa
+import SafariServices
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Override point for customization after application launch.
+        
+        let manager = SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: "safari.Wallet.Extension") { state, error in
+            guard let state = state else {
+                guard state == nil else {
+                    print("Error fetching extension state: \(error!.localizedDescription)")
+                    return
+                }
+                print("Unknown error fetching extension state")
+                return
+            }
+            if state.isEnabled == true {
+                print("extension is enabled")
+            } else {
+                print("extension is disabled")
+            }
+        }
+        
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
