f7f66c587daae16ab1b152aabb16a7c836c8f192
natclark <39421782+natclark@users.noreply.github.com>
2021-10-14 14:27:11 -0500

    add simple message signing implementation

--- test-dapp/src/script.js
+++ test-dapp/src/script.js
@@ -7,10 +7,12 @@ window.addEventListener(`load`, () => {
 
     const $ = (id) => document.getElementById(id);
 
+    let currentAccounts = [];
+
     $(`connect`).addEventListener(`click`, async () => {
         if (typeof window.ethereum !== `undefined`) {
             try {
-                const currentAccounts = await window.ethereum.request({ method: `eth_requestAccounts`, });
+                currentAccounts = await window.ethereum.request({ method: `eth_requestAccounts`, });
                 $(`wallet`).innerText = currentAccounts[0];
             } catch (e) {
                 alert(`Something went wrong with eth_requestAccounts.`);
@@ -22,8 +24,59 @@ window.addEventListener(`load`, () => {
         }
     });
 
-    $(`sign`).addEventListener(`click`, () => {
-        alert(`Message signing is yet to be implemented.`);
+    $(`sign`).addEventListener(`click`, async () => {
+        if (currentAccounts.length > 0) {
+            try {
+                const EIP712Domain = [
+                    { name: `name`, type: `string`, },
+                    { name: `version`, type: `string`, },
+                    { name: `chainId`, type: `uint256`, },
+                    { name: `verifyingContract`, type: `address`, },
+                    { name: `salt`, type: `bytes32`, },
+                ];
+                const Echo = [
+                    { name: `message`, type: `string`, },
+                    { name: `bidder`, type: `Identity`, },
+                ];
+                const Identity = [
+                    { name: `userId`, type: `uint256`, },
+                    { name: `wallet`, type: `address`, }
+                ];
+                const domain = {
+                    name: `Safari Wallet Test dApp`,
+                    version: `2`,
+                    chainId: 1,
+                    verifyingContract: ``, // TODO
+                    salt: ``, // TODO
+                };
+                const message = {
+                    amount: `Hello World`,
+                    bidder: {
+                        userId: 1337,
+                        wallet: currentAccounts[0],
+                    }
+                };
+                const data = JSON.stringify({
+                    types: {
+                        EIP712Domain,
+                        Echo,
+                        Identity,
+                    },
+                    domain,
+                    primaryType: `Echo`,
+                    message,
+                });
+                await window.ethereum.request({
+                    method: `eth_signTypedData_v3`,
+                    params: [currentAccounts[0], data],
+                    from: currentAccounts[0],
+                });
+            } catch (e) {
+                alert(`Something went wrong with eth_sign.`);
+            }
+        } else {
+            alert(`Please connect a wallet first.`);
+        }
     });
 
 });
