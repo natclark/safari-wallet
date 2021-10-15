83ef6afd9fe322c82d7eb355c7b7297d4ce8540f
Tassilo von Gerlach <tassilo.v.gerlach@gmail.com>
2021-10-15 16:37:11 -0400

    added dependemcy and fetch balance

--- Shared (App)/TransactionsTab/TransactionsView.swift
+++ Shared (App)/TransactionsTab/TransactionsView.swift
@@ -6,13 +6,45 @@
 //
 
 import SwiftUI
+import web3
 
 struct TransactionsView: View {
-    var body: some View {
-       Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).task {
-          print("Hello Transaction")
-       }
-    }
+   
+   private enum Constants {
+      static let clientUrl: String = "Your_AlchemyApi_OR_Infura_URL"
+      static let ethAccountAddress: String? = nil
+   }
+   
+   @State private var displayText: String = "Loading balance"
+   
+   let ethClient: EthereumClient
+   
+   init() {
+      guard let clientUrl = URL(string: Constants.clientUrl) else { fatalError("You need to set a valid url") }
+      self.ethClient = EthereumClient(url: clientUrl)
+   }
+   
+   var body: some View {
+      Text(displayText).task {
+         self.fetchAccountBalance()
+      }
+   }
+   
+   func fetchAccountBalance() {
+      guard let address = Constants.ethAccountAddress else {
+         self.displayText = "Make sure to set an account address"
+         return
+      }
+      ethClient.eth_getBalance(address: EthereumAddress(address),
+                                block: EthereumBlock(rawValue: "latest")) { error, value in
+         guard let value = value else {
+            self.displayText = "There was an error loading your balance \(String(describing: error))"
+            if let error = error { print(error) }
+            return
+         }
+         self.displayText = "Balance: \(value) Gwei for account \(address)"
+      }
+   }
 }
 
 struct TransactionsView_Previews: PreviewProvider {
